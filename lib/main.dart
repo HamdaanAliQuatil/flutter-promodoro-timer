f331335de8ef0e37abb894274ae1fb253d02191b
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-05-27 10:27:49 +0530

    feat: add contact screen

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,18 @@ class Home extends StatelessWidget {
     return Container(
       width: sizeX,
       height: sizeY,
-      child: Stack(
+      child: ListView(
+        children: showContacts(),
+      ),
+      // child: ListView.separated(
+      //   itemBuilder:((context, index) => createSquare(index)),
+      //   separatorBuilder: (context, index) => createSeparator(index),
+      //   itemCount: 25,
         // verticalDirection: VerticalDirection.up,
         // textDirection: TextDirection.rtl,
-        children:showPizzaLayout(sizeX, sizeY),
-      ),
+        // children:createSquares(25),
+      //   scrollDirection: Axis.vertical,
+      // ),
     );
   }
 
@@ -44,20 +51,38 @@ class Home extends StatelessWidget {
     List<Widget> squares = [];
     squares.add(Container(color: Colors.black,));
     while( i<numSquares){
-      Positioned square = Positioned(
-        top: 100 + i.toDouble()*100,
-        left: 40 + i.toDouble()*30,
-        // alignment: Alignment.bottomRight,
-        child: Container(
-        color: colors[i],
-        width: 60.0 * (numSquares-i),
-        height: 60.0 *(numSquares - i),
+      Container square = Container(
+        color: colors[i%5],
+        width: 100.0,
+        height: 100.0,
         child: Text(i.toString()),
-      ));
+      );
       i++;
       squares.add(square);
     }
     return squares;
+  }
+
+  Widget createSquare(int position){
+
+    List colors = [Colors.amber, Colors.deepPurple, Colors.deepOrange, Colors.indigo, Colors.lightBlue];
+  
+      Container square = Container(
+        color: colors[position%5],
+        width: 100.0,
+        height: 100.0,
+        child: Text(position.toString()),
+      );
+
+    return square;
+  }
+
+  Widget createSeparator(int position){
+    Widget seperator = Container(
+      height: 15,
+      color: Colors.black,
+    );
+    return seperator;
   }
 
   List<Widget> showPizzaLayout(double sizeX, double sizeY){
@@ -70,6 +95,7 @@ class Home extends StatelessWidget {
         ),
       ),
     );
+    layoutChildren.add(backGround);
     Positioned pizzaCard = Positioned(
       top: sizeY/20,
       left: sizeX/20,
@@ -101,9 +127,67 @@ class Home extends StatelessWidget {
         ),
       ),
     );
-    layoutChildren.add(backGround);
     layoutChildren.add(pizzaCard);
+    Positioned buttonOrder = Positioned(
+      bottom: sizeY /20,
+      left: sizeX /20,
+      width: sizeX - sizeX/10,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.orange[900],
+          elevation: 12,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        child: Text('Order Now',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+      ),
+      onPressed: (){},
+      ),
+      );
+      layoutChildren.add(buttonOrder);
     return layoutChildren;
   }
 
+  List<Contact> buildContacts(){
+    List<Contact> contacts = [];
+    contacts.add(Contact('Thomas Anderson', 'The Matrix', Icons.sentiment_neutral));
+    contacts.add(Contact('Frank Slade', 'Scent of a woman', Icons.sentiment_satisfied));
+    contacts.add(Contact('Mildred Hayes','Three Billboards outside Ebbing, NMissouri',Icons.sentiment_dissatisfied));
+    contacts.add(Contact('Bruce Wayne', 'The Dark Knight', Icons.sentiment_neutral));
+    contacts.add(Contact('Jamal Malik', 'The Millionaire', Icons.sentiment_neutral));
+    return contacts;
+  }
+
+  List<ListTile> showContacts(){
+    List<Contact> contacts = buildContacts();
+    for(int i=0; i<20; i++){
+      contacts.addAll(buildContacts());
+    }
+    List<ListTile> list = [];
+    contacts.forEach((contact) {
+      list.add(ListTile(
+        title: Text(contact.name),
+        subtitle: Text(contact.subtile),
+        leading: CircleAvatar(
+          child: Icon(contact.icon),
+          backgroundColor: Colors.amber[600],
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () => true,
+      ));
+     });
+     return list;
+  }
+}
+
+class Contact{
+  String name;
+  String subtile;
+  IconData icon;
+  Contact(this.name, this.subtile, this.icon);
 }
